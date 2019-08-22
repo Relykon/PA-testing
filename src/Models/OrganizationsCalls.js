3943cd829886c62e81f610aff53a6757ea9e2ca0
Kyle Harrington <harrington.kyle@gmail.com>
2019-08-22 14:28:56 -0700

    attempt to wrap getOrganization call in searchOrganizations call

--- src/Models/OrganizationsCalls.js
+++ src/Models/OrganizationsCalls.js
@@ -1,38 +1,58 @@
 const OrganizationsCalls = {};
 
-// const API_KEY = process.env.REACT_APP_API_KEY;
-// const ID = process.env.APP_ID;
+const API_KEY = process.env.REACT_APP_API_KEY;
+const ID = process.env.REACT_APP_ID;
 
 OrganizationsCalls.searchOrganizations = function searchOrganizations() {
     console.log('searchOrganizations')
-    const url = `https://api.data.charitynavigator.org/v2/Organizations?app_id=2c89e638&app_key=d3c949aec82ec22b164ff99c22503f6d&pageSize=10&rated=true&state=WA&city=Seattle`;
+    const url = `https://api.data.charitynavigator.org/v2/Organizations?app_id=${ID}&app_key=${API_KEY}&pageSize=10&rated=true&state=WA&city=Seattle`;
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'            
+        }
+    })
+    .then(result => result.json())
+    .then(data => OrganizationsCalls.getOrganizationDetails(data))
+    .catch(err => Error(err, "Loading Organizations"));
+}
+
+OrganizationsCalls.getOrganization = function getOrganization(ein) {
+    console.log('about to fetch')
+    const url = `https://api.data.charitynavigator.org/v2/Organizations/${ein}?app_id=${ID}&app_key=${API_KEY}`;
     return fetch(url, {
         method: 'GET',
         headers: {
             'Accept': 'application/json'
-            // 'apikey': API_KEY,
-            // 'id': ID
         }
     })
-    .then(result => result.json())
-    .then(data => OrganizationsCalls.simplifyOrganizations(data))
-    .catch(err => Error(err, "Loading Organizations"));
+    .then(result => {
+        if (result.status === 200) return result.json()
+    })
+    .then(data => OrganizationsCalls.simplifyOrganization(data))
+    .catch(err => Error(err, "Loading Organization"));
 }
 
-OrganizationsCalls.simplifyOrganizations = function simplifyOrganizations(organizations) {
-    return organizations.map(organization => {
-         const orgDetails = {
-             charityName: organization.charityName,
-             ein: organization.ein,
-             tagLine: organization.tagLine,
-             mission: organization.mission,
-             currentRating: organization.currentRating.rating,
-             website: organization.websiteURL,
-             cause: organization.cause.causeName,
-             category: organization.category.categoryName
-         };
-         return orgDetails;
-    })
+OrganizationsCalls.getOrganizationDetails = function getOrganizationDetails(organizations) {
+    return organizations.map(organization => OrganizationsCalls.getOrganization(organization.ein))
+};
+
+OrganizationsCalls.simplifyOrganization = function simplifyOrganization(organization) {
+    console.log(organization, 'ooo')
+    const orgDetails = {
+        charityName: organization.charityName,
+        ein: organization.ein,
+        tagLine: organization.tagLine,
+        mission: organization.mission,
+        currentRating: organization.currentRating.score,
+        website: organization.websiteURL,
+        cause: organization.cause.causeName,
+        category: organization.category.categoryName,
+        city: organization.mailingAddress.city,
+        state: organization.mailingAddress.stateOrProvince,
+        phone: organization.phoneNumber
+    };
+    return orgDetails;
 };
 
 export default OrganizationsCalls;
--- src/components/Organizations/Organizations.js
+++ src/components/Organizations/Organizations.js
@@ -6,36 +6,48 @@ class Organizations extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            organizations: []
+            organizations: [],
+            organization: {}
         };
     }
 
     loadOrganizations() {
         OrganizationsCalls.searchOrganizations()
             .then(orgs => {
-                console.log(orgs, 'Organizations')
-                console.log(this.state.organizations, 'L17')
+                // console.log(orgs)
                 this.setState({ organizations: orgs });
-                console.log(this.state.organizations, 'L19')
             })
             .catch(err => Error(err, "Loading Organizations"));
     }
 
+    loadOrganization() {
+        // OrganizationsCalls.getOrganization()
+        // .then(org => {
+        //     console.log(org)
+        //     this.setState({ organization: org });
+        // })
+        // .catch(err => Error(err, "Loading Organization"));
+    }
+
     componentDidMount() {
         this.loadOrganizations();
+        this.loadOrganization();
     }
 
     render() {
 
         let organizationsList = this.state.organizations.map(organization => (
             <div className="OrganizationsList">
+                <hr/>
                 <h3>{organization.charityName}</h3>
+                <p>{organization.city}, {organization.state}</p>
                 <h4>{organization.tagLine}</h4>
                 <p>{organization.mission}</p>
-                <p>Category: {organization.category}</p>
-                <p>Current Rating: {organization.currentRating}</p>
-                <p>Cause: {organization.cause}</p>
-                <a href={organization.websiteURL}>{organization.charityName}'s Website</a>
+                <p><b>Category: </b>{organization.category}</p>
+                <p><b>Current Rating: </b>{organization.currentRating}</p>
+                <p><b>Cause: </b>{organization.cause}</p>
+                <a href={organization.website}>{organization.charityName}'s Website</a>
+                <p>Phone Number: {organization.phone}</p>
             </div>
         ));
         console.log(this.state.organizations, 'L30')
